5788acc994f2295e757268a5381456488ecb89f3
dpupov <dpupov@sitronics.com>
2024-11-29 09:20:16 +0500

    addition keycloak admin

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,5 @@
 import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
-import {AppService} from './app.service';
 import {UserModule} from '../user/user.module';
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import configurations from "../../configurations";
@@ -8,11 +7,12 @@ import {SequelizeModule} from "@nestjs/sequelize";
 import {User} from "../user/models/user.model";
 import {WatchlistModule} from "../watchlist/watchlist.module";
 import {Watchlist} from "../watchlist/models/watchlist.model";
-import {AuthGuard, KeycloakConnectModule, ResourceGuard, RoleGuard} from "nest-keycloak-connect";
-import {APP_GUARD} from "@nestjs/core";
+import {KeycloakModule} from "../keycloak/keycloak.module";
+import {AppService} from "./app.service";
 
 @Module({
     imports: [
+        KeycloakModule,
         UserModule,
         ConfigModule.forRoot({
             isGlobal: true,
@@ -34,37 +34,10 @@ import {APP_GUARD} from "@nestjs/core";
             })
         }),
         WatchlistModule,
-        KeycloakConnectModule.register({
-            authServerUrl: 'http://localhost:8080',
-            realm: 'geofocus-realm',
-            clientId: 'nest-app',
-            secret: 'mVYcnM4eZ99mWiFAjnXil8CZFcyOOwMc',
-        }),
 
     ],
+    providers: [AppService],
     controllers: [AppController],
-    providers: [
-        AppService,
-
-        //Это добавляет защиту аутентификации на глобальном уровне, вы также можете ограничить ее область действия, если хотите.
-        //Возвращает ошибку 401 «не авторизовано», если не удается проверить токен JWT или отсутствует заголовок Bearer.
-        {
-            provide: APP_GUARD,
-            useClass: AuthGuard,
-        },
-        //Это добавляет глобальный уровень ресурсной защиты.
-        //Только контроллеры, аннотированные @Resource, и методы с @Scopes обрабатываются этой защитой.
-        {
-            provide: APP_GUARD,
-            useClass: ResourceGuard,
-        },
-        //Это добавляет глобальный уровень ролевой защиты.
-        //Используется декоратором `@Roles` с необязательным декоратором `@AllowAnyRole` для разрешения любой указанной переданной роли.
-        {
-            provide: APP_GUARD,
-            useClass: RoleGuard,
-        },
-    ],
 })
 export class AppModule {
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,35 +1,10 @@
 import {Controller, Get} from '@nestjs/common';
-import {Public, Roles, Unprotected} from "nest-keycloak-connect";
 
 @Controller()
 export class UserController {
 
-    @Get('kc/unprotected')
-    @Unprotected()
-    getNoGuard(): string {
-        return `hello from no guard`;
-    }
-
-    @Get('kc/user')
-    @Roles({roles: ['my_user']})
-    getUser(): string {
-        return `hello from user`;
-    }
-
     @Get('kc/admin')
-    @Roles({roles: ['my_admin']})
     getAdmin(): string {
         return `hello from admin`;
-    }
-
-    @Get('kc/all')
-    getAll(): string {
-        return `hello from authenticated`;
-    }
-
-    @Get('kc/public')
-    @Public()
-    getPublic(): string {
-        return `hello from public`;
     }
 }
